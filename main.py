81d4b7f916333b928c1cc5034419fcc6f03f87d7
Faisal Aziz <aziz.faisal247@gmail.com>
2024-11-26 23:55:11 +0100

    Added Customisable Tabs to the application and changed the name to Productivity App

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem, TabbedPanelHeader
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
@@ -35,23 +36,85 @@ class CustomColorLabel(Label):
         self.rect.size = instance.size
         self.rect.pos = instance.pos
 
-class HabitTrackerApp(App):
+class CustomTabbedPanel(TabbedPanel):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.bind(current_tab=self.update_tab_colors)
+
+    def update_tab_colors(self, *args):
+        for header in self.tab_list:
+            if isinstance(header, TabbedPanelHeader):
+                if header.state == "down":
+                    header.background_color = (0, 0.39, 0, 1) # Dark Green Colour
+                else:
+                    header.background_color = (0.9, 0.9, 0.9, 1)
+
+class ProductivityApp(App):
     def build(self):
         # Root Layout
-        self.root_layout = CustomBoxLayout(orientation='vertical', padding=10, spacing=10)
+        root_layout = CustomBoxLayout(orientation="vertical", padding=10, spacing=10)
 
-        # Title Label
+        # Title Label for the Main Screen
         title_label = CustomColorLabel(
-            bg_color=(0.5, 0.5, 0.2, 1),
-            text="Habit Tracker",
+            bg_color=(0.5, 0.5, 0.2, 1),  # Olive Green Background
+            text="Productivity App",
             font_size='24sp',
             size_hint=(1, None),
-            height=50,
+            height=60,
             halign='center',
             valign='middle'
         )
-        title_label.bind(size=title_label.setter('text_size'))  # Align text properly
-        self.root_layout.add_widget(title_label)
+        title_label.bind(size=title_label.setter('text_size'))  # Center the text
+        root_layout.add_widget(title_label)
+
+        # Tabbed Panel
+        tab_panel = CustomTabbedPanel(
+            tab_pos = 'bottom_left',
+            tab_height = 50,
+            background_color=(0.9, 0.9, 0.9, 1)
+        )
+        tab_panel.do_default_tab = False
+        
+        # Adjust the tab width
+        tab_panel.bind(size=self.adjust_tab_width)
+
+        # Habit Tracker Tab
+        habit_tab = self.create_habit_tracker_tab()
+        tab_panel.add_widget(habit_tab)
+
+        # To-Do List Tab
+        todo_tab = self.create_todo_list_tab()
+        tab_panel.add_widget(todo_tab)
+
+        root_layout.add_widget(tab_panel)
+
+        return root_layout
+
+    
+
+
+    def adjust_tab_width(self, instance, value):
+        num_tabs = len(instance.tab_list)
+        if num_tabs > 0:
+            instance.tab_width = instance.width/num_tabs
+
+    def create_habit_tracker_tab(self):
+
+        habit_tab = TabbedPanelItem(text="Habit Tracker")
+        layout = CustomBoxLayout(orientation='vertical', padding=10, spacing=10)
+
+        # # Title Label
+        # title_label = CustomColorLabel(
+        #     bg_color=(0.5, 0.5, 0.2, 1),
+        #     text="Habit Tracker",
+        #     font_size='24sp',
+        #     size_hint=(1, None),
+        #     height=50,
+        #     halign='center',
+        #     valign='middle'
+        # )
+        # title_label.bind(size=title_label.setter('text_size'))  # Align text properly
+        # layout.add_widget(title_label)
 
         # Input box to add a new habit
         input_layout = BoxLayout(size_hint=(1, None), height=50, spacing=10)
@@ -60,16 +123,25 @@ class HabitTrackerApp(App):
         add_button.bind(on_press=self.add_habit)
         input_layout.add_widget(self.habit_input)
         input_layout.add_widget(add_button)
-        self.root_layout.add_widget(input_layout)
+        layout.add_widget(input_layout)
 
         # Scrollable area for habits
         self.habit_scroll = ScrollView(size_hint=(1, 1))
         self.habit_list = BoxLayout(orientation='vertical', size_hint_y=None, spacing=10)
         self.habit_list.bind(minimum_height=self.habit_list.setter('height'))  # Adjust size for scrolling
         self.habit_scroll.add_widget(self.habit_list)
-        self.root_layout.add_widget(self.habit_scroll)
+        layout.add_widget(self.habit_scroll)
 
-        return self.root_layout
+        habit_tab.add_widget(layout)
+        return habit_tab
+
+    def create_todo_list_tab(self):
+
+        todo_tab = TabbedPanelItem(text="To-Do List")
+        layout = CustomBoxLayout(orientation='vertical', padding=10, spacing=10)
+
+        todo_tab.add_widget(layout)
+        return todo_tab
     
     def add_habit(self, instance):
         habit_text = self.habit_input.text.strip()
@@ -121,4 +193,4 @@ class HabitTrackerApp(App):
     def delete_habit(self, instance):
         self.habit_list.remove_widget(instance.parent)
     
-HabitTrackerApp().run()
+ProductivityApp().run()
