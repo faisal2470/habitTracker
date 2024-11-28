9a2387bca8536a6bf4404ca009873ca76c681dcf
Faisal Aziz <aziz.faisal247@gmail.com>
2024-11-28 11:59:08 +0100

    Added the CustomTabbedPanel and the Home Tab to view the calendar on app start.

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.boxlayout import BoxLayout 
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem, TabbedPanelHeader
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.scrollview import ScrollView
 from kivy.graphics import Color, Rectangle, RoundedRectangle
+import calendar as cal
+import datetime as dt
 
 class CustomBoxLayout(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         # Add a Canvas instruction for the background
         with self.canvas.before:
-            Color(0.7, 0.7, 0.7, 1) # Light Blue Background
+            Color(0.7, 0.7, 0.7, 1) # Light Grey Background
             self.rect = Rectangle(size=self.size, pos=self.pos)
         # Bind size and position updates to keep the background in place
         self.bind(size=self._rect_update, pos=self._rect_update)
@@ -39,13 +42,18 @@ class CustomColorLabel(Label):
 class CustomTabbedPanel(TabbedPanel):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        # Set a consistent background color for the tab content area
+        self.background_color = (0.7, 0.7, 0.7, 1)  # Light Grey Background
+        self.background_normal = ""  # Disable default background texture
+
         self.bind(current_tab=self.update_tab_colors)
 
     def update_tab_colors(self, *args):
         for header in self.tab_list:
             if isinstance(header, TabbedPanelHeader):
                 if header.state == "down":
-                    header.background_color = (0, 0.39, 0, 1) # Dark Green Colour
+                    header.background_color = (0.7, 0.7, 0.7, 1) # Grey Colour
                 else:
                     header.background_color = (0.9, 0.9, 0.9, 1)
 
@@ -70,13 +78,15 @@ class ProductivityApp(App):
         # Tabbed Panel
         tab_panel = CustomTabbedPanel(
             tab_pos = 'bottom_left',
-            tab_height = 50,
-            background_color=(0.9, 0.9, 0.9, 1)
+            tab_height = 50
         )
-        tab_panel.do_default_tab = False
-        
+
         # Adjust the tab width
         tab_panel.bind(size=self.adjust_tab_width)
+
+        # Home Tab
+        home_tab = self.create_home_tab()
+        tab_panel.add_widget(home_tab)
 
         # Habit Tracker Tab
         habit_tab = self.create_habit_tracker_tab()
@@ -86,21 +96,82 @@ class ProductivityApp(App):
         todo_tab = self.create_todo_list_tab()
         tab_panel.add_widget(todo_tab)
 
+        # Set Default Tab as False
+        tab_panel.do_default_tab = False
+
+        # Manually select the Home Tab as the default tab
+        tab_panel.switch_to(home_tab)
+
+        # Trigger an initial update of tab colors
+        tab_panel.update_tab_colors()
+
         root_layout.add_widget(tab_panel)
 
         return root_layout
 
     
+    def create_home_tab(self):
+
+        home_tab = TabbedPanelItem(text="Home")
+        home_tab.bind(size=home_tab.setter("text_size"))
+        layout = CustomBoxLayout(orientation='vertical')
+        scroll_view = ScrollView(size_hint=(1, 1))
+
+        header_row = GridLayout(cols=8, size_hint_y=None, height=30)
+        days = cal.day_abbr[:]
+        column = ['Time'] + days
+        
+        for col in column:
+            header_row.add_widget(
+                Label( 
+                    text=col, 
+                    color=(0, 0, 0, 1),
+                    bold=True,
+                    size_hint_y=None, 
+                    height=40,
+                    halign='center',
+                    valign='middle'
+                )
+            )
+        layout.add_widget(header_row)
+
+        self.time_row = GridLayout(cols=8, size_hint_y=None)
+        for t in [dt.time(i).strftime('%H:%M') for i in range(24)]:
+            time_label = Label(
+                text=t,
+                size_hint=(1, None),
+                height=30,
+                color = (0, 0, 0, 1)
+            )
+            self.time_row.add_widget(time_label)
+            
+            for _ in range(7):
+                cell = Label(
+                    text="",
+                    size_hint=(1, None),
+                    height=30,
+                    color=(0, 0, 0, 1)
+                )
+                self.time_row.add_widget(cell)
+        
+        self.time_row.bind(minimum_height=self.time_row.setter("height"))
+        scroll_view.add_widget(self.time_row)
+
+        layout.add_widget(scroll_view)
+        home_tab.add_widget(layout)
+        return home_tab
+
 
 
     def adjust_tab_width(self, instance, value):
         num_tabs = len(instance.tab_list)
         if num_tabs > 0:
-            instance.tab_width = instance.width/num_tabs
+            instance.tab_width = instance.width/num_tabs - 2
 
     def create_habit_tracker_tab(self):
 
         habit_tab = TabbedPanelItem(text="Habit Tracker")
+        habit_tab.bind(size=habit_tab.setter("text_size"))
         layout = CustomBoxLayout(orientation='vertical', padding=10, spacing=10)
 
         # # Title Label
@@ -138,6 +209,7 @@ class ProductivityApp(App):
     def create_todo_list_tab(self):
 
         todo_tab = TabbedPanelItem(text="To-Do List")
+        todo_tab.bind(size=todo_tab.setter("text_size"))
         layout = CustomBoxLayout(orientation='vertical', padding=10, spacing=10)
 
         todo_tab.add_widget(layout)
