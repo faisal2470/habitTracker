9a7eee3a3998f504bc00135ca07357b9b214164a
Faisal Aziz <aziz.faisal247@gmail.com>
2024-12-10 17:32:29 +0100

    Added the create_tables and the close function in DatabaseManager

--- db_manager.py
+++ db_manager.py
@@ -1,13 +1,25 @@
 import sqlite3
 
-
 class DatabaseManager:
     def __init__(self, db_name='productivity_app.db'):
         self.db_name = db_name
-        self.connection = None
+        self.conn = None
 
     def connect(self):
-        if self.connection is None:
-            self.connection = sqlite3.connect(self.db_name)
+        if self.conn is None:
+            self.conn = sqlite3.connect(self.db_name)
         else:
             pass
+
+    def create_tables(self):
+        self.connect()
+        self.cursor = self.conn.cursor()
+        self.close()
+
+    def close(self):
+        if self.conn:
+            self.conn.close()
+            self.conn = None
+        else:
+            pass
+
